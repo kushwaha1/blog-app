82d472fe7bc7e57042120045c4f4cccae41f7c2d
Sweety <rsweet375@gmail.com>
2026-03-02 00:40:05 +0530

    add backend live url on client side

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://blog-app-5yy6.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
